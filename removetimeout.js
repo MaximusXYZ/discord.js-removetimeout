98b8741e06cbd5d1c504503e1d2633b3b8d4ce43
Maximus <85184014+MaximusXYZ@users.noreply.github.com>
2022-01-08 20:48:53 +0300

    Update removetimeout.js

--- removetimeout.js
+++ removetimeout.js
@@ -4,37 +4,29 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 		const fetch = require('node-fetch');
 		const ms = require('ms');
-		const time = args.slice(1).join(' ');
 		const user = message.mentions.users.first();
+
 		if(!user) return message.channel.send('Kullanıcı etiketle!');
-		if(!time) return message.channel.send('Süreyi Belirt! `1m`');
-
-		const milliseconds = ms(time);
-		if(!milliseconds || milliseconds < 10000 || milliseconds > 2419200000) {
-			return message.channel.send('10s / 24d arası bir süre dilimi giriniz.');
-		}
-
-		const iosTime = new Date(Date.now() + milliseconds).toISOString();
 
 		await fetch(`https://discord.com/api/guilds/${message.guild.id}/members/${user.id}`, {
 			method: 'PATCH',
-			body: JSON.stringify({ communication_disabled_until: iosTime }),
+			body: JSON.stringify({ communication_disabled_until: 0 }),
 			headers: {
 				'Content-Type': 'application/json',
 				'Authorization': `Bot ${client.token}`,
 			},
 		});
-		message.channel.send(`${user.username} isimli kişi \`${time}\`boyunca zaman aşımına uğradı!`);
+		message.channel.send(`${user.username} isimli kişinin zaman aşımı kalktı.`);
 	},
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
-	aliases: ['removetimeout', 'rt'],
+	aliases: ['removetimeout', 'rtimeout'],
 	permLevel: 2
 };
 	exports.help = {
-		name: 'rtimeout',
-		description: 'zaman aşımını kaldırıyo gari',
-		usage: 'rtimeout @etiket'
+		name: 'rt',
+		description: 'İstediğiniz şeyi bota yazdırır.',
+		usage: 'yaz [yazdırmak istediğiniz şey]'
 	  };
   
